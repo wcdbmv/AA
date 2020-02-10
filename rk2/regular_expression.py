41185f28aa6cdfbc0f20f574e33f8f6028e3b951
Kerimov Ahmed <kerimov.dev@yandex.ru>
2020-02-10 12:32:30 +0300

    Improve rk2

--- rk2/regular_expression.py
+++ rk2/regular_expression.py
@@ -2,14 +2,25 @@ import re
 from main import main
 
 
+def clean_line(line: str) -> str:
+    def replacement(m):
+        return '' if m.group(1) else m.group(0)
+    pattern = re.compile(r'(/\*.*?\*/)')
+    without_multiline_comments = pattern.sub(replacement, line)
+    pattern = re.compile(r'([^"\\]*(\\.|"([^"\\]*\\.)*[^"\\]*"))*')
+    without_strings_and_multiline_comments = pattern.sub(replacement, without_multiline_comments)
+    pattern = re.compile(r'//.*$')
+    return pattern.sub('', without_strings_and_multiline_comments)
+
+
 def test_method(line: str) -> bool:
-    pattern = re.compile(r'(\".*\")*[^\"]*\.\w+\(')
-    return bool(pattern.search(line))
+    pattern = re.compile(r'\.\w+\(')
+    return bool(pattern.search(clean_line(line)))
 
 
 def test_std(line: str) -> bool:
-    pattern = re.compile(r'((\".*\")*.*\Wstd::)|(^std::)')
-    return bool(pattern.search(line))
+    pattern = re.compile(r'\bstd::')
+    return bool(pattern.search(clean_line(line)))
 
 
 if __name__ == '__main__':
